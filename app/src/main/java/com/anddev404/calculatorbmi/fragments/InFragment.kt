aa6b3c5f7e1b6adf7c8dffcc6fb2d178a4c7d2b6
anddev404 <marcinek07@gmail.com>
2022-10-19 22:42:41 +0200

    add fragment for in

---
+++ app/src/main/java/com/anddev404/calculatorbmi/fragments/InFragment.kt
@@ -0,0 +1,23 @@
+package com.anddev404.calculatorbmi.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.anddev404.calculatorbmi.R
+
+class InFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_in, container, false)
+    }
+}
